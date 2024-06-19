c023a87fd61f326d70b8cfc2acef946a8986de7b
Annu <annudev626@gmail.com>
2024-06-18 22:03:35 -0600

    initial commit for DIwithSpringBoot

---
+++ DIwithSpringBoot/src/main/java/com/example/DIwithSpringBoot/Dev.java
@@ -0,0 +1,13 @@
+package com.example.DIwithSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("working on Awesome Project");
+
+    }
+
+}
