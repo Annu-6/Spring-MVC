99f30d8a0e56fd61c55d9d2ce930b1ed53f02a31
Annu-6 <annudev626@gmail.com>
2024-06-19 01:41:15 -0600

    Added DIwithSpringBoot

---
+++ DIwithSpringBoot/src/test/java/com/example/DIwithSpringBoot/DIwithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DIwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DIwithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
